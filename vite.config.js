5ca4565f371d7ea353ec7b161edc44b0234a36d9
BeatlesBOB <beatlesbob74120@gmail.com>
2022-11-24 10:31:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
     react(),
     VitePWA(
       { 
-        registerType: 'autoUpdate'
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        }
       }
     )
   ]
